c535a49d39c1be7821586477432dde3368f51fbe
Vlad <vladonnx@mail.ru>
2021-09-13 16:31:43 +0300

    Add Beaty View card. Delete .Vue Component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <v-app>
-    <v-navigation-drawer absolute temporary v-model="drawer">
-      <v-list></v-list>
+    <v-navigation-drawer
+      absolute
+      temporary
+      v-model="drawer"
+      class="hidden-md-and-up"
+    >
+      <v-list>
+        <v-item-group>
+          <v-item>
+            <v-btn
+              @click="dialog = !dialog"
+              v-for="(item, i) in menuItems"
+              text
+              dense
+              :key="`menuitem${i}`"
+            >
+              <v-icon left v-html="item.icon"></v-icon>
+              {{ item.title }}</v-btn
+            >
+          </v-item>
+        </v-item-group>
+      </v-list>
     </v-navigation-drawer>
     <v-app-bar app dark>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-app-bar-nav-icon>
+
       <v-text-field v-text="'Quotes'"></v-text-field>
       <v-spacer></v-spacer>
-      <v-item-group>
+      <v-item-group class="hidden-sm-and-down">
         <v-item>
           <v-btn
             @click="dialog = !dialog"
@@ -20,6 +45,7 @@
         </v-item>
       </v-item-group>
     </v-app-bar>
+    <!-- <app-header></app-header> -->
     <v-main>
       <v-item-group>
         <v-container>
@@ -61,38 +87,53 @@
               <v-item>
                 <v-card elevation="2">
                   <v-card-title>Автор: {{ item.author }}</v-card-title>
-                  <v-card-subtitle>{{ item.created_at }}</v-card-subtitle>
-                  <v-card-subtitle>{{ item.tags.name }}</v-card-subtitle>
-                  <v-card-text>{{ item.text }}</v-card-text>
+                  <v-card-subtitle>{{
+                    BeautyDate(item.created_at)
+                  }}</v-card-subtitle>
+                  <!-- <v-card-subtitle >{{item.tags }}</v-card-subtitle> -->
+                  <div class="d-flex flex-row sm-1">
+                    <v-subheader>#</v-subheader>
+                    <v-subheader
+                      v-for="(tag, t) in item.tags"
+                      :key="`item.tags${t}`"
+                      >{{ tag.name }}</v-subheader
+                    >
+                  </div>
+
+                  <v-card-text
+                    ><strong>Текст:</strong> {{ item.text }}</v-card-text
+                  >
                 </v-card>
               </v-item>
             </v-col>
           </v-row>
           <v-row>
             <v-toolbar dense flat>
-              <v-toolbar-items>
-                <v-btn
-                  @click="LoadPage(response.prev_page_url)"
-                  text
-                  flat
-                  v-show="response.prev_page_url"
-                  >Предыдущая страница</v-btn
-                >
-                <v-spacer></v-spacer>
-                <v-btn
-                  @click="LoadPage(response.next_page_url)"
-                  text
-                  flat
-                  left
-                  v-show="response.next_page_url"
-                  >Следующая страница</v-btn
-                >
-              </v-toolbar-items>
               <v-toolbar-title left
-                >Страница: {{ response.current_page }}/{{
-                  response.last_page
-                }}</v-toolbar-title
-              >
+                >{{ response.current_page }}
+                <!-- /{{
+                    response.last_page
+                  }} -->
+              </v-toolbar-title>
+              <v-container>
+                <v-toolbar-items>
+                  <v-btn
+                    @click="LoadPage(response.prev_page_url)"
+                    text
+                    v-if="response.prev_page_url"
+                    >Назад</v-btn
+                  >
+
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    @click="LoadPage(response.next_page_url)"
+                    text
+                    left
+                    v-if="response.next_page_url"
+                    >Вперед</v-btn
+                  >
+                </v-toolbar-items>
+              </v-container>
             </v-toolbar>
           </v-row>
         </v-container>
@@ -110,64 +151,72 @@
     </v-main>
     <!-- MODAL INPUT -->
     <v-dialog v-model="dialog" width="500">
-      <v-card>
-        <v-card-title>
-          <span class="text-h5">Добавить цитату</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col>
-                <v-textarea
-                  v-model="postData.text"
-                  label="Текст"
-                  required
-                  hint="Напишите текст вашей цитату сюда."
-                ></v-textarea>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  v-model="postData.author"
-                  label="Автор"
-                  hint="Ваше имя, псевдоним или прозвище."
-                ></v-text-field>
-              </v-col>
+      <v-form ref="form" v-model="valid">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Добавить цитату</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col>
+                  <!-- <validation-provider name="Name" rules="required|max:10"> -->
+                  <v-textarea
+                    v-model="postData.text"
+                    label="Текст"
+                    :rules="RequiredRules"
+                    required
+                    hint="Напишите текст вашей цитату сюда."
+                  ></v-textarea>
+                  <!-- </validation-provider> -->
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    v-model="postData.author"
+                    label="Автор"
+                    :rules="RequiredRules"
+                    required
+                    hint="Ваше имя, псевдоним или прозвище."
+                  ></v-text-field>
+                </v-col>
 
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  v-model="postData.tags"
-                  :items="[
-                    'Skiing',
-                    'Ice hockey',
-                    'Soccer',
-                    'Basketball',
-                    'Hockey',
-                    'Reading',
-                    'Writing',
-                    'Coding',
-                    'Basejump',
-                  ]"
-                  label="Тэги"
-                  multiple
-                  hint="Добавьте теги. Миниму один, максимум 3"
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*Все поля обязательны к заполнению</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="dialog = false">
-            Закрыть
-          </v-btn>
-          <v-btn color="blue darken-1" text @click="PostQuote()">
-            Добавить
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    @click="GetTagList"
+                    v-model="postData.tags"
+                    :items="tags"
+                    :rules="MaxItemRules"
+                    counter="3"
+                    item-text="name"
+                    item-value="id"
+                    required
+                    label="Тэги"
+                    multiple
+                    hint="Добавьте теги. Миниму один, максимум 3"
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*Все поля обязательны к заполнению</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Закрыть
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              :disabled="!valid"
+              text
+              @click="PostQuote()"
+            >
+              Добавить
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-form>
     </v-dialog>
   </v-app>
 </template>
@@ -184,9 +233,26 @@ export default {
         },
       ];
     },
+    // TagName(){
+
+    // },
   },
 
   methods: {
+    BeautyDate(date) {
+      let d = new Date(date);
+      let serialized =
+        d.getDay() +
+        "." +
+        d.getMonth() +
+        "." +
+        d.getFullYear() +
+        " " +
+        d.getHours() +
+        ":" +
+        d.getMinutes();
+      return serialized;
+    },
     Load(response) {
       if (response.data.status) {
         // this.msg = response.data.msg;
@@ -197,6 +263,15 @@ export default {
         this.snackbar = true;
         setTimeout(this.OutputClear, 5000);
       }
+    },
+
+    GetTagList() {
+      this.axios({
+        method: "get",
+        url: "http://127.0.0.1:8000/api/tags",
+        responseType: "json",
+      }).then((response) => (this.tags = response.data.data));
+      // .finally(() => (this.loading = false));
     },
     OutputClear() {
       this.errors = null;
@@ -212,18 +287,23 @@ export default {
         .finally(() => (this.loading = false));
     },
     PostQuote() {
-      this.loading = true;
-      this.axios
-        .post("http://127.0.0.1:8000/api/post", this.postData)
+      if (this.postData.text && this.postData.author && this.postData.tags) {
+        this.loading = true;
+        this.axios
+          .post("http://127.0.0.1:8000/api/post", this.postData)
 
-        // ({
-        //   method: "post",
-        //   url: "http://127.0.0.1:8000/api/post",
-        //   responseType: "json",
-        // })
-        .then((response) => this.Load(response))
-        .finally(() => (this.loading = false));
-      this.dialog = false;
+          // ({
+          //   method: "post",
+          //   url: "http://127.0.0.1:8000/api/post",
+          //   responseType: "json",
+          // })
+          .then((response) => this.Load(response))
+          .finally(() => (this.loading = false));
+        this.dialog = false;
+      } else {
+        this.msg = "Заполните все поля!";
+        this.snackbar = true;
+      }
     },
   },
   mounted() {
@@ -239,6 +319,7 @@ export default {
   data: function () {
     return {
       response: null,
+      valid: false,
       msg: null,
       errors: null,
       snackbar: false,
@@ -246,12 +327,25 @@ export default {
       drawer: false,
       loading: false,
       dialog: false,
+      RequiredRules: [
+        (v) => !!v || "Обязательное поле",
+      ],
+      MaxItemRules:[
+        v =>(v.length <= 3) || "Не менее одного и не более трех"
+
+      ],
+      tags: [],
       postData: {
         text: null,
         author: null,
         tags: [],
       },
     };
+  },
+  watch: {
+    group() {
+      this.drawer = false;
+    },
   },
 };
 </script>
